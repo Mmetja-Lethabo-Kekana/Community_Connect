1fb23e05e4bca74703b3b3a0eb6b98fa38b231e4
Mmetja Lethabo Kekana <mmetjalkekana@gmail.com>
2026-08-24 15:09:25 +0200

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'your-repo-name' with the actual name of your GitHub repository
 export default defineConfig({
   plugins: [react()],
+  base: '/Community_Connect/', 
 })
